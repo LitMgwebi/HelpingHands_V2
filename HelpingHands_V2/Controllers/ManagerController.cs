72884e43304cbd8825ad5e3014d9a0c94847e2e4
LitMgwebi <lithim11@gmail.com>
2023-11-13 22:56:37 +0200

    PDF code

--- HelpingHands_V2/Controllers/ManagerController.cs
+++ HelpingHands_V2/Controllers/ManagerController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Diagnostics.Contracts;
 
 namespace HelpingHands_V2.Controllers
@@ -216,5 +217,61 @@ namespace HelpingHands_V2.Controllers
                 return View();
             }
         }
+
+        //public void GeneratePDF()
+        //{
+        //    var outputFileName = Path.GetTempFileName();
+        //    PdfDocument pdfDocument = new PdfDocument(new PdfWriter(new FileStream(outputFileName, FileMode.Create, FileAccess.Write)));
+        //    Document document = new Document(pdfDocument);
+
+        //    try
+        //    {
+        //        Paragraph heading = new Paragraph("REGEX Suppliers");
+        //        heading.SetUnderline(0.5f, -1f);
+        //        document.Add(heading);
+
+        //        Table table = new Table(2, true);
+        //        List<TblSupplier> Suppliers = _userService.GetSuppliers();
+
+        //        Cell cell = new Cell().Add(new Paragraph("Supplier Name"));
+        //        cell.SetBold();
+        //        table.AddCell(cell);
+        //        cell = new Cell().Add(new Paragraph("Supplier Email"));
+        //        cell.SetBold();
+        //        table.AddCell(cell);
+        //        foreach (var x in Suppliers)
+        //        {
+        //            var SuppName = new Paragraph(x.SupplierName);
+        //            var SuppEmail = new Paragraph(x.SupplierEmail);
+
+        //            var column1 = new Cell().Add(SuppName);
+        //            var column2 = new Cell().Add(SuppEmail);
+        //            table.AddCell(column1);
+        //            table.AddCell(column2);
+        //        }
+        //        DateTime currentDate = DateTime.Now;
+        //        Paragraph dateParagraph = new Paragraph("Created Date: " + currentDate.ToString("yyyy-MM-dd"));
+        //        dateParagraph.SetHorizontalAlignment(iText.Layout.Properties.HorizontalAlignment.LEFT);
+        //        document.Add(dateParagraph);
+        //        document.Add(table);
+
+        //        Paragraph disclaimer = new Paragraph("Disclaimer: The contents of this pdf are for reference only.");
+        //        document.Add(disclaimer);
+
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //    finally
+        //    {
+        //        document.Close();
+        //        Response.ContentType = "application/pdf";
+        //        Response.AppendTrailer("content-disposition",
+        //            "attachment;filename=Suppliers.pdf");
+        //        Response.SendFileAsync(outputFileName);
+        //    }
+        //}
     }
 }
