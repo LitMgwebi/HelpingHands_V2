7ef50fab49bb7d0e3c696d8bac031b1a1f1ee787
LitMgwebi <lithim11@gmail.com>
2023-12-13 14:44:19 +0200

    Implementing Condtion Model on Condition Views

--- HelpingHands_V2/obj/Debug/net6.0/HelpingHands_V2.AssemblyInfo.cs
+++ HelpingHands_V2/obj/Debug/net6.0/HelpingHands_V2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72884e43304cbd8825ad5e3014d9a0c94847e2e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
