e65f02b04e38a3c959a46cfc04cc922c06777cf8
LitMgwebi <lithim11@gmail.com>
2024-02-16 10:56:04 +0200

    Fixing images classes

--- HelpingHands_V2/obj/Debug/net6.0/HelpingHands_V2.AssemblyInfo.cs
+++ HelpingHands_V2/obj/Debug/net6.0/HelpingHands_V2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e3af05fdb7831938ad06a018260e1efbe4c19d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6e58a72a560bdc65d6e0898d2fad2e9c6829f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
