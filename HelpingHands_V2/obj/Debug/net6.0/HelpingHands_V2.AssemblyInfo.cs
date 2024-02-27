377dcee069f804a951ee06168d1a414fde555e63
LitMgwebi <lithim11@gmail.com>
2024-02-27 16:42:06 +0200

    Edits to navbar

--- HelpingHands_V2/obj/Debug/net6.0/HelpingHands_V2.AssemblyInfo.cs
+++ HelpingHands_V2/obj/Debug/net6.0/HelpingHands_V2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cde29ef70478102d12eb90104790dde1f237af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1148ec49668a47f2904dfd3ccf456e1af27492")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpingHands_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
